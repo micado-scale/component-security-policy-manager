2487bf0a3a1f749c73a211df62f0dcc6b0e85518
Hai Van <hvandang@outlook.com>
2018-10-05 11:35:40 +0100

    update test functions as apis change

--- lib/CredStoreLibrary.py
+++ lib/CredStoreLibrary.py
@@ -17,18 +17,6 @@ class CredStoreLibrary(object):
         self._status = ''
         self._data = ''
 
-    '''def print_hello(self):
-        url     = 'http://127.0.0.1:5003/v1.0/'
-        res = requests.get(url)
-        self._status = res.status_code'''
-
-    def add_a_secret(self, name, value):
-        url     = 'http://127.0.0.1:5003/v1.0/secrets'
-        payload = {'name': name, 'value': value}
-        res = requests.post(url, data=payload)
-        json_data = json.loads(res.text)
-        self._status = json_data['code']     
-
     def status_should_be(self, expected_status):
         if expected_status != str(self._status):
             raise AssertionError("Expected status to be '%s' but was '%s'."
@@ -39,25 +27,57 @@ class CredStoreLibrary(object):
             raise AssertionError("Expected data to be '%s' but was '%s'."
                                % (expected_data, self._data))
 
-    def delete_a_secret(self, secretname=None,):
+    def add_a_secret(self, name, value):
         url     = 'http://127.0.0.1:5003/v1.0/secrets'
-        payload = {'name': secretname}
-        res = requests.delete(url, data=payload)
+        payload = {'name': name, 'value': value}
+        res = requests.post(url, json=payload)
+        json_data = json.loads(res.text)
+        self._status = json_data['code']     
+
+    def update_a_secret(self,secretname,newvalue):
+        url     = 'http://127.0.0.1:5003/v1.0/secrets/' + secretname
+        payload = {'value': newvalue}
+        res = requests.put(url, json=payload)
+        json_data = json.loads(res.text)
+        self._status = json_data['code'] 
+
+    def delete_a_secret(self, secretname=None,):
+        url     = 'http://127.0.0.1:5003/v1.0/secrets/' + secretname
+        res = requests.delete(url)
         json_data = json.loads(res.text)
         self._status = json_data['code']
 
     def read_a_secret(self, secretname=None):
-        url     = 'http://127.0.0.1:5003/v1.0/secrets'
-        payload = {'name': secretname}
-        res = requests.get(url, data=payload)
+        url     = 'http://127.0.0.1:5003/v1.0/secrets/' + secretname
+        res = requests.get(url)
         json_data = json.loads(res.text)
         self._status = json_data['code']
         if(self._status == http_code_ok):
             self._data = json_data['data']['secret_value']
 
     def init_a_vault(self, shares, threshold):
-        url     = 'http://127.0.0.1:5003/v1.0/vault'
+        url     = 'http://127.0.0.1:5003/v1.0/vaults'
         payload = {'shares': shares, 'threshold': threshold}
-        res = requests.post(url, data=payload)
+        res = requests.post(url, json=payload)
         json_data = json.loads(res.text)
-        self._status = json_data['code'] 
+        self._status = json_data['code']
+
+    def create_app_secret(self, secretname, secretvalue, servicename):
+        url     = 'http://127.0.0.1:5003/v1.0/appsecrets'
+        payload = {'name': secretname, 'value': secretvalue, 'service': servicename}
+        res = requests.post(url, json=payload)
+        json_data = json.loads(res.text)
+        self._status = json_data['code']
+
+    def delete_app_secret(self, secretname, servicename):
+        url     = 'http://127.0.0.1:5003/v1.0/appsecrets/' + secretname
+        payload = {'service': servicename}
+        res = requests.delete(url, json=payload)
+        json_data = json.loads(res.text)
+        self._status = json_data['code']
+
+    def retrieve_app_secret_id(self, secretname):
+        url     = 'http://127.0.0.1:5003/v1.0/appsecrets/' + secretname
+        res = requests.get(url)
+        json_data = json.loads(res.text)
+        self._status = json_data['code']

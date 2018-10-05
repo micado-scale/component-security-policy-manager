a5e6733cf0ed2c0693ba321e2d3d83b0ad74f428
Hai Van <hvandang@outlook.com>
2018-10-05 11:34:47 +0100

    update api routes to comply restful

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,16 @@
 from app import app, dksecrets
 from app import vaultclient
+from flask_restful import Api
 
-'''def index():
-	"""[summary]
-	Hello world function
-	[description]
-	This function is only for testing if the web service is in operating
-	"""
-	return "Hello, World!"
-   
-app.add_url_rule('/v1.0/','index',index)'''
+api = Api(app)
 
 # Application sensitive information management
-#app.add_url_rule('/v1.0/create_secret','create_secret_api', dksecrets.create_secret_api, methods=['POST'])
-app.add_url_rule('/v1.0/appsecrets','add_docker_secret_api', dksecrets.add_secret_api, methods=['POST'])
+api.add_resource(dksecrets.Dksecrets,'/v1.0/appsecrets')
+api.add_resource(dksecrets.Dksecret,'/v1.0/appsecrets/<secret_name>')
 
 # Infrastructure sensitive information management
-app.add_url_rule('/v1.0/vault','initvault', vaultclient.init_vault_api, methods=['POST'])
+api.add_resource(vaultclient.Secret,'/v1.0/secrets/<secret_name>')
+api.add_resource(vaultclient.Secrets,'/v1.0/secrets')
 
-# Add or update secret
-app.add_url_rule('/v1.0/secrets','write_secret_api', vaultclient.write_secret_api, methods=['POST','PUT'])
-app.add_url_rule('/v1.0/secrets','read_secret_api', vaultclient.read_secret_api, methods=['GET'])
-app.add_url_rule('/v1.0/secrets','delete_secret_api', vaultclient.delete_secret_api, methods=['DELETE'])
+api.add_resource(vaultclient.Vaults,'/v1.0/vaults')
 

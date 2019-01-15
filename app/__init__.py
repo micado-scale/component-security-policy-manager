c63c2b1aab3550ab429fd3f4b005502fb868c9b4
Peter Bauer <peter.bauer@balasys.hu>
2019-01-15 11:49:03 +0100

    fix(logging): removing unused log handler

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ Init module
 The init module creates Flask object and log handler
 '''
 import logging
-from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_restful import Api
 from app.secrets import Secrets
@@ -16,6 +15,9 @@ from app.image_verify import ImageVerify
 
 
 app = Flask(__name__)
+
+app.logger.setLevel(logging.DEBUG)
+
 api = Api(app)
 api.add_resource(Secrets, '/v1.0/secrets', '/v1.0/secrets/<secret_name>')
 api.add_resource(NodeCerts, '/v1.0/nodecerts', '/v1.0/nodecerts/<serial>')
@@ -23,13 +25,3 @@ api.add_resource(NodeCrl, '/v1.0/nodecrl')
 api.add_resource(JoinTokens, '/v1.0/jointokens', '/v1.0/jointokens/<token>')
 api.add_resource(CryptoEngine, '/v1.0/cryptoengine/<path:path>')
 api.add_resource(ImageVerify, '/v1.0/imageverify')
-
-logHandler = RotatingFileHandler('error.log', maxBytes=1000, backupCount=1)
-logHandler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(module)s - %(funcName)s - %(lineno)d - '
-    '%(levelname)s - %(message)s')
-logHandler.setFormatter(formatter)
-
-app.logger.setLevel(logging.ERROR)
-app.logger.addHandler(logHandler)

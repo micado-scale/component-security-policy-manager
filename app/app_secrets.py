f50fe6ebefa4b636058deaee690ba404479e1a01
Peter Bauer <peter.bauer@balasys.hu>
2019-01-24 18:13:52 +0100

    feat(app_secrets): CRUD

--- app/app_secrets.py
+++ app/app_secrets.py
@@ -2,7 +2,7 @@ import logging
 from flask import request
 from flask import Response
 from flask_restful import Resource
-from lib.kubernetes_backend import KubernetesBackend, KubernetesBackendError
+from lib.kubernetes_backend import KubernetesBackend, KubernetesBackendError, KubernetesBackendKeyNotFoundError
 
 
 class AppSecrets(Resource):
@@ -36,22 +36,72 @@ class AppSecrets(Resource):
 
         return Response('Created', 201)
 
-    # def delete(self, token):
-    #     '''[summary]
-    #     Delete a join token.
-    #     [description]
-    #     Deletes a join token from Kubernetes.
+    def get(self, secret_name):
+        '''[summary]
+        Read a secret from kubernetes
+        [description]
 
-    #     Arguments:
-    #         [type] string -- [description] client's join token
-    #     '''
-    #     self._logger.info('Join Tokens endpoint method DELETE token %s from %s', token, request.remote_addr)
+        Returns:
+            [type] string -- [description] the secret value
+        '''
+        self._logger.info('App Secrets endpoint method GET secret "%s" from %s', secret_name, request.remote_addr)
 
-    #     try:
-    #         res = subprocess.run(['kubeadm', 'token', 'delete', token], capture_output=True)
-    #     except Exception as error:
-    #         self._logger.error('Unable to call kubeadm.')
-    #         self._logger.info(error)
-    #         return Response('Unable to delete Kubernetes token.', 500)
+        if not secret_name:
+            return Response('Unable to read Kubernetes secret, missing parameters.', 400)
 
-    #     return Response(res.stdout, 200)
+        try:
+            secret = self._kubernetes_backend.read_secret(secret_name)
+        except KubernetesBackendKeyNotFoundError:
+            return Response('Kubernetes secret not found.', 404)
+        except KubernetesBackendError as error:
+            self._logger.error('Unable to read Kubernetes secret.')
+            self._logger.info(error)
+            return Response('Unable to read Kubernetes secret.', 500)
+
+        return Response(secret, 200)
+
+    def put(self, secret_name):
+        '''[summary]
+        Update a secret in kubernetes
+        [description]
+
+        Arguments:
+            secret_name {[type]} -- [description] Name of secret
+        '''
+        self._logger.info('App Secrets endpoint method PUT secret "%s" from %s', secret_name, request.remote_addr)
+
+        json_body = request.json
+        secret_value = json_body['value']
+
+        if not secret_name or not secret_value:
+            return Response('Unable to update Kubernetes secret, missing parameters.', 400)
+
+        try:
+            secret = self._kubernetes_backend.update_secret(secret_name)
+        except KubernetesBackendKeyNotFoundError:
+            return Response('Secret not found.', 404)
+        except KubernetesBackendError as error:
+            self._logger.error('Unable to update Kubernetes secret.')
+            self._logger.info(error)
+            return Response('Unable to update Kubernetes secret.', 500)
+
+        return Response('Updated', 200)
+
+    def delete(self, secret_name):
+        '''[summary]
+        Delete a secret from kubernetes
+        [description]
+        '''
+        self._logger.info('App Secrets endpoint method DELETE secret "%s" from %s', secret_name, request.remote_addr)
+
+        if not secret_name:
+            return Response('Unable to delete Kubernetes secret, missing parameters.', 400)
+
+        try:
+            self._kubernetes_backend.delete_secret(secret_name)
+        except KubernetesBackendError as error:
+            self._logger.error('Unable to delete Kubernetes secret.')
+            self._logger.info(error)
+            return Response('Unable to delete Kubernetes secret.', 500)
+
+        return Response('Deleted', 200)
